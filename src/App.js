f1dc636fb5fe9f6103aac83713d15ade19625a9d
David Mwendwa <klatzdavie19@gmail.com>
2021-07-27 10:51:19 +0300

    feat: add color helper to generate shades of colors

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import Palette from './Palette';
 import seedColors from './seedColors';
+import { generatePalette } from './ColorHelpers';
 
 import './App.css';
 
 const App = () => {
-  //console.log(...seedColors);
+  console.log(generatePalette(seedColors[4]));
   return (
-    
     <div>
-      <Palette palette={seedColors[4]} />
+      <Palette palette={seedColors[2]} />
     </div>
   );
-}
+};
 
 export default App;
