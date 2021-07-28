5a969b08c1ba8be7cba80ddd66d490c6958aafce
David Mwendwa <klatzdavie19@gmail.com>
2021-07-28 10:42:59 +0300

    feat: finish styling miniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -3,40 +3,59 @@ import { withStyles } from '@material-ui/styles';
 
 const styles = {
   root: {
-    backgroundColor: "white",
-    border: "1px solid black",
-    borderRadius: "5px",
-    padding: "0.5rem",
-    position: "relative",
-    overflow: "hidden",
-    "&:hover": {
-      cursor: "pointer"
-    }
+    backgroundColor: 'white',
+    border: '1px solid black',
+    borderRadius: '5px',
+    padding: '0.5rem',
+    position: 'relative',
+    overflow: 'hidden',
+    '&:hover': {
+      cursor: 'pointer',
+    },
   },
   colors: {
-    backgroundColor: "grey"
+    backgroundColor: "#dae1e4",
+    height: "150px",
+    width: "100%",
+    borderRadius: "5px",
+    overflow: "hidden"
   },
   title: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    margin: "0",
-    color: "black",
-    paddingTop: ".5rem",
-    fontSize: "1rem",
-    position: "relative"
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: '0',
+    color: 'black',
+    paddingTop: '.5rem',
+    fontSize: '1rem',
+    position: 'relative',
   },
   emoji: {
-    marginLeft: ".5rem",
-    fontSize: "1.5rem"
-  }
+    marginLeft: '.5rem',
+    fontSize: '1.5rem',
+  },
+  miniColor: {
+    height: '25%',
+    width: '20%',
+    display: 'inline-block',
+    margin: '0 auto',
+    position: "relative",
+    marginBottom: "-6px"
+  },
 };
 
 const MiniPalette = (props) => {
-  const { classes, paletteName, emoji } = props;
+  const { classes, paletteName, emoji, colors } = props;
+  const miniColoxBoxes = colors.map((color) => (
+    <div
+      className={classes.miniColor}
+      style={{ backgroundColor: color.color }}
+      key={color.name}
+    />
+  ));
   return (
     <div className={classes.root}>
-      <div className={classes.colors}></div>
+      <div className={classes.colors}>{miniColoxBoxes}</div>
       <h5 className={classes.title}>
         {paletteName} <span className={classes.emoji}>{emoji}</span>
       </h5>
