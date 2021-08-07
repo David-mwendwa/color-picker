0ca0ab09ff159b38625169fee84ebcb32508bc80
David Mwendwa <klatzdavie19@gmail.com>
2021-08-07 07:48:06 +0300

    feat: make paletteList responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes';
+
 const styles = {
   root: {
     backgroundColor: 'blue',
@@ -12,24 +14,37 @@ const styles = {
     alignItems: 'flex-start',
     flexDirection: 'column',
     flexWrap: 'wrap',
+    [sizes.down('xl')]: {
+      width: '80%',
+    },
+    [sizes.down('sm')]: {
+      width: '75%',
+    },
   },
   nav: {
     display: 'flex',
     width: '100%',
     justifyContent: 'space-between',
-    alignItems: "center",
+    alignItems: 'center',
     color: 'white',
-    "& a": {
-      color: "white"
-    }
+    '& a': {
+      color: 'white',
+    },
   },
   palettes: {
     boxSizing: 'box-sizing',
     width: '100%',
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 30%)',
-    gridGap: '5%',
+    gridGap: '1.5rem',
+    [sizes.down('md')]: {
+      gridTemplateColumns: 'repeat(2, 50%)',
+    },
+    [sizes.down('xs')]: {
+      gridTemplateColumns: 'repeat(1, 100%)',
+      gridGap: '1rem',
+    },
   },
 };
 
-export default styles
+export default styles;
--- src/styles/sizes.js
+++ src/styles/sizes.js
@@ -5,6 +5,7 @@ const sizes = {
       sm: '768px',
       md: '992px',
       lg: '1200px',
+      xl: '1600px'
     };
     return `@media (max-width: ${sizes[size]})`;
   },
